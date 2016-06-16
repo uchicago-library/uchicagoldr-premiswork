fcb87db15a2d2a5ecd2ee5b0975f9233e92b5449
Brian Balsamo <balsamo@uchicago.edu>
2016-06-16 15:08:01 -0500

    Changing the version number to denote the beta status.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'pypremis',
-    version = '1.0.0',
+    version = '1.0.0b',
     url = 'https://github.com/uchicago-library/uchicagoldr-premiswork',
     author = "Brian Balsamo <balsamo@uchicago.edu>",
     author_email = "balsamo@uchicago.edu",
